6205aa1ff96198c153ffc02f6282d59502f21467
Nicolas <nickcintra@hotmail.com>
2023-02-07 13:10:20 -0300

    fim migration SQLServer

--- Program.cs
+++ Program.cs
@@ -90,6 +90,8 @@ public class Product
     public int Id { get; set; }
     public string Code { get; set; }
     public string Name { get; set; }
+    
+    public string Description { get; set; }
 }
 
 public class ApplicationDbContext : DbContext
