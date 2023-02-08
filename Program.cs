7013f5affd7a446f10e8385770f487e245b145df
Nicolas <nickcintra@hotmail.com>
2023-02-07 21:34:58 -0300

    inserindo tags se existirem

--- Program.cs
+++ Program.cs
@@ -3,21 +3,30 @@ using Microsoft.AspNetCore.Mvc;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSqlServer<ApplicationDbContext>(builder.Configuration["Database:SqlServer"]);
 
- 
+
 var app = builder.Build();
 var configuration = app.Configuration;
 ProductRepository.Init(configuration);
 
 // ENDPOINT INSERIR
 app.MapPost("/products", (ProductRequest productRequest, ApplicationDbContext context) =>
-{   
+{
     var category = context.Categories.Where(c => c.Id == productRequest.CategoryId).First();
-    var product = new Product {
+    var product = new Product
+    {
         Code = productRequest.Code,
         Name = productRequest.Name,
         Description = productRequest.Description,
         Category = category
     };
+    if (productRequest.Tags != null)
+    {
+        product.Tags = new List<Tag>();
+        foreach (var item in productRequest.Tags)
+        {
+            product.Tags.Add(new Tag { Name = item });
+        }
+    }
     context.Products.Add(product);
     context.SaveChanges();
     return Results.Created($"/products/{product.Id}", product.Id);
